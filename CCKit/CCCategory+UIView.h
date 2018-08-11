bffaf80b8c83a18119d3b8b0d41754e8092bd5a9
胡超 <thinkorz@foxmail.com>
2018-08-12 04:40:17 +0800

    更新UIViewCategory

--- CCKit/CCCategory+UIView.h
+++ CCKit/CCCategory+UIView.h
@@ -36,9 +36,17 @@ typedef NS_ENUM(NSInteger,CCBorderMask) {
 /** 背景图片，拉伸平铺*/
 @property (nonatomic,strong) UIImage *backgroundImage;
 
-
 @property (nonatomic,readonly) void(^addSubview)(UIView *view);
+@property (nonatomic,readonly) void(^insertSubviewAtIndex)(UIView *view, NSInteger atIndex);
+@property (nonatomic,readonly) void(^insertSubviewAboveSubview)(UIView *view, UIView *aboveView);
+@property (nonatomic,readonly) void(^insertSubviewBelowSubview)(UIView *view, UIView *belowView);
+@property (nonatomic,readonly) void(^mas_makeConstraints)(void(^)(MASConstraintMaker *make));
+@property (nonatomic,readonly) void(^mas_remakeConstraints)(void(^)(MASConstraintMaker *make));
+@property (nonatomic,readonly) void(^mas_updateConstraints)(void(^)(MASConstraintMaker *make));
 @property (nonatomic,readonly) void(^addSubviewMaker)(UIView *view, void(^)(MASConstraintMaker *make));
+@property (nonatomic,readonly) void(^insertSubviewAtIndexMaker)(UIView *view, NSInteger atIndex, void(^)(MASConstraintMaker *make));
+@property (nonatomic,readonly) void(^insertSubviewAboveSubviewMaker)(UIView *view, UIView *aboveView, void(^)(MASConstraintMaker *make));
+@property (nonatomic,readonly) void(^insertSubviewBelowSubviewMaker)(UIView *view, UIView *belowView, void(^)(MASConstraintMaker *make));
 @property (nonatomic,readonly) void(^removeAllSubviews)(void);
 
 #pragma mark - Border
