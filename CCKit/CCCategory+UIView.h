13bc75222e83013286b791ec1137b1b130537c68
胡超 <thinkorz@foxmail.com>
2018-12-01 00:42:24 +0800

    UIView新增x,y get/set方法

--- CCKit/CCCategory+UIView.h
+++ CCKit/CCCategory+UIView.h
@@ -24,6 +24,8 @@ typedef NS_ENUM(NSInteger,CCBorderMask) {
 
 @property (nonatomic,assign) CGPoint origin;
 @property (nonatomic,assign) CGSize  size;
+@property (nonatomic,assign) CGFloat x;
+@property (nonatomic,assign) CGFloat y;
 @property (nonatomic,assign) CGFloat width;
 @property (nonatomic,assign) CGFloat height;
 @property (nonatomic,assign) CGFloat top;
