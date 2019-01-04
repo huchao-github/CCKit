d021edc8dec913f9dc85e0eb72ae30f48e77159c
胡超 <thinkorz@foxmail.com>
2019-01-04 19:04:00 +0800

    UIImage新增fromColorEx方法

--- CCKit/CCCategory+UIImage.h
+++ CCKit/CCCategory+UIImage.h
@@ -11,8 +11,10 @@
 @interface UIImage (CCKit)
 
 @property (class,readonly) UIImage *clearImage;
-
+/** 根据颜色创建一个1x1大小的纯色图片*/
 @property (class,readonly) UIImage *(^fromColor)(UIColor *color);
+/** 根据颜色创建一个指定size大小的纯色图片，并根据cornerRadius裁剪圆角*/
+@property (class,readonly) UIImage *(^fromColorEx)(UIColor *color, CGSize size, CGFloat cornerRadius);
 
 @property (nonatomic,readonly) UIImage *(^tintColor)(UIColor *color);
 
