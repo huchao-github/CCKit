3b6e84914bdc348a0e08e7b793871139d7e795b1
胡超 <thinkorz@foxmail.com>
2019-04-22 22:47:27 +0800

    1.2.0新增NSString拼音扩展  优化UIImage颜色参数，可以是UIColor和NSString(颜色16进制字符串，#FF0000)类型

--- CCKit/CCCategory+NSString.h
+++ CCKit/CCCategory+NSString.h
@@ -15,9 +15,13 @@ typedef NS_ENUM(uint32_t, CCDESOptions) {
 
 @interface NSString (CCKit)
 
+/** md5值*/
 @property (readonly) NSString *md5Str;
+/** 文件md5值, 注意: self必须是一个文件路径*/
 @property (readonly) NSString *fileMd5Str;
+/** URL编码*/
 @property (readonly) NSString *URLEncodedStr;
+/** URL解码*/
 @property (readonly) NSString *URLDecodedStr;
 @property (readonly) NSString *(^addURLParameters)(NSDictionary *parameters);
 /** DES加解密 padding手动填充0*/
@@ -25,5 +29,12 @@ typedef NS_ENUM(uint32_t, CCDESOptions) {
 @property (readonly) NSString *(^desDecrypt)(CCDESOptions options, BOOL padding, void *key, void *iv);
 /** 格式化日期*/
 @property (readonly) NSDate *(^formatToDate)(NSString *dateFormat);
+
+/** 是否包含中文*/
+@property (readonly) BOOL chinese;
+/** 拼音*/
+@property (readonly) NSString *pinyinStr;
+/** 拼音首字母*/
+@property (readonly) NSString *pinyinFirstWord;
 
 @end
--- CCKit/CCCategory+UIImage.h
+++ CCKit/CCCategory+UIImage.h
@@ -10,13 +10,28 @@
 
 @interface UIImage (CCKit)
 
+/**
+ * 创建一个1x1大小的透明颜色的图片
+ */
 @property (class,readonly) UIImage *clearImage;
-/** 根据颜色创建一个1x1大小的纯色图片*/
-@property (class,readonly) UIImage *(^fromColor)(UIColor *color);
-/** 根据颜色创建一个指定size大小的纯色图片，并根据cornerRadius裁剪圆角*/
-@property (class,readonly) UIImage *(^fromColorEx)(UIColor *color, CGSize size, CGFloat cornerRadius);
 
-@property (nonatomic,readonly) UIImage *(^tintColor)(UIColor *color);
+/**
+ * 根据颜色创建一个1x1大小的纯色图片
+ * 参数color为UIColor类型或者NSString类型，如过传NSString类型，则格式为@"#FF0000"，颜色16进制字符串
+ */
+@property (class,readonly) UIImage *(^fromColor)(id color);
+
+/**
+ * 根据颜色创建一个指定size大小的纯色图片，并根据cornerRadius裁剪圆角
+ * 参数color为UIColor类型或者NSString类型，如过传NSString类型，则格式为@"#FF0000"，颜色16进制字符串
+ */
+@property (class,readonly) UIImage *(^fromColorEx)(id color, CGSize size, CGFloat cornerRadius);
+
+/**
+ * 给UIImage着色
+ * 参数color为UIColor类型或者NSString类型，如过传NSString类型，则格式为@"#FF0000"，颜色16进制字符串
+ */
+@property (nonatomic,readonly) UIImage *(^tintColor)(id color);
 
 @property (nonatomic,readonly) UIImage *(^scaleToSize)(CGSize size);
 
